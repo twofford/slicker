fd8ba87d92f974531cb00e4f59d23ff0bbedc64b
twofford <taylor.wofford@gmail.com>
2024-10-22 23:59:35 -0400

    update routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
 interface Routes {
   getUserChannels: string;
   getChannelMessages: string;
+  getAllUsers: string;
+  createUser: string;
 }
 
 const routes: Routes = {
   getUserChannels: "/channels/:userId",
   getChannelMessages: "/messages/:channelId",
+  getAllUsers: "/users",
+  createUser: "/users"
 };
 
 export default routes;
