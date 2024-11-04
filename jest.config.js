e7636f5ef1b5be0c4daa5c17e56423008a3dd831
twofford <taylor.wofford@gmail.com>
2024-11-03 23:49:18 -0500

    add tests for channel model

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   testEnvironment: "node",
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-  },
+    '^.+\\.tsx?$': 'ts-jest',
+},
+moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: [
+    "**/__tests__/**/*.js?(x)", // Match only .js or .jsx files
+    "**/?(*.)+(spec|test).js?(x)", // Match files ending with .spec.js, .test.js, etc.
+  ],
 };
